46b54f7a8d548c14527c94360b2a9e7d9d568fd0
Levi Bibo and Luke Philips <luke@>
2016-11-07 10:26:28 -0800

    refactor and convert all functions to methods of board object

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
-var gridWidth;
 
 function Board() {
   this.grid = [];
+  this.adjacentBlanks = [];
+  this.checked =  [];
   this.minesRemaining;
-  this.width;
+  this.gridWidth;
 }
 
 Board.prototype.placeMines = function() {
   var potentialLocations = [];
 
-  for (row = 0; row < this.width; row++) {
-    for (column = 0; column < this.width; column++) {
+  for (row = 0; row < this.gridWidth; row++) {
+    for (column = 0; column < this.gridWidth; column++) {
       potentialLocations.push(row.toString() + column.toString());
     }
   }
@@ -24,16 +25,15 @@ Board.prototype.placeMines = function() {
 //add mine warning numbers to grid
 Board.prototype.setMineWarnings = function() {
   //loop through grid
-  for (row = 0; row < this.width; row++) {
-    for (column = 0; column < this.width; column++) {
+  for (row = 0; row < this.gridWidth; row++) {
+    for (column = 0; column < this.gridWidth; column++) {
       //check for mine at that location
       if (this.grid[row][column] === "X") {
         //loop through adjacent squares
         for (rowAdjust = -1; rowAdjust < 2; rowAdjust++) {
           for (columnAdjust = -1; columnAdjust < 2; columnAdjust++) {
             //test to make sure the selected square is within the grid
-            if ((row+rowAdjust >= 0) && (row+rowAdjust <= this.width-1) && (column+columnAdjust >= 0) && (column+columnAdjust <= this.width-1)) {
-              console.log("count one");
+            if ((row+rowAdjust >= 0) && (row+rowAdjust <= this.gridWidth-1) && (column+columnAdjust >= 0) && (column+columnAdjust <= this.gridWidth-1)) {
               //test to make sure the selected square doesn't also contain a mine
               if (this.grid[row+rowAdjust][column+columnAdjust] != "X") {
                 //increment selected square by one
@@ -48,129 +48,106 @@ Board.prototype.setMineWarnings = function() {
 }
 
 Board.prototype.resetGrid = function() {
-  this.grid = [];
-  for (row = 0; row < this.width; row++) {
+  this.clearArray();
+  for (row = 0; row < this.gridWidth; row++) {
     this.grid.push([]);
-    for (column = 0; column < this.width; column++) {
+    for (column = 0; column < this.gridWidth; column++) {
       this.grid[row].push(0);
     }
   }
-  clearArray(adjacentBlanks);
-  clearArray(checked);
 }
 
 Board.prototype.updateUI = function() {
   $(".grid").empty();
-  for (row = 0; row < this.width; row++) {
+  for (row = 0; row < this.gridWidth; row++) {
     $(".grid").append('<div class="row gridRow" id="row' + row.toString() + '"></div>');
-    for (column = 0; column < this.width; column++) {
-      $("#row" + row).append('<div class="gridColumn" id="' + row.toString()  + column.toString() + '">'+this.grid[row][column]+'</div>');
-        if (this.grid[row][column]) {
-          $("#"+row.toString()+column.toString()).toggleClass("hasMine");
-        }
-      $("#" + row.toString() + column.toString()).click(function() {
+    for (column = 0; column < this.gridWidth; column++) {
+      var squareCoordinateID = "#" + row.toString() + "-" + column.toString()
+      $("#row" + row).append('<div class="gridColumn" id="' + row.toString() + "-"  + column.toString() + '">'+this.grid[row][column]+'</div>');
+        // if (this.grid[row][column]) {
+        //   $("#"+row.toString()+column.toString()).toggleClass("hasMine");
+        // }
+      $(squareCoordinateID).click(function() {
         // $(this).toggleClass("coordinateHighlight");
-        pushAdjacents(this.id);
-        loopThroughBoard();
-        clearArray(checked);
+        gameBoard.pushAdjacents(squareCoordinateID.slice(1, squareCoordinateID.length).split("-"));
+        gameBoard.loopThroughBoard();
+        gameBoard.clearArray();
       })
     }
   }
 }
 
-var adjacentBlanks = [];
-var adjacentSquares = [];
-var checked =  [];
+Board.prototype.pushAdjacents = function(coordinates) {
+  var x = parseInt(coordinates[1]);
+  var y = parseInt(coordinates[0]);
+  up = [x, (y - 1)];
+  down = [x, (y + 1)];
+  left = [(x - 1), y];
+  right = [(x + 1),y];
 
-function pushAdjacents(coordinates) {
-  var x = getX(coordinates);
-  var y = getY(coordinates);
-  up = [(x - 1), y]
-  down = [(x + 1), y]
-  left = [x, (y - 1)]
-  right = [x, (y + 1)]
-
-  if (isInBounds(up) === true && isNotMine(up) === true) {
-    if (checked.indexOf("" + up) === -1) {
-      adjacentBlanks.push(up);
+  if (this.isInBounds(up) === true && this.isNotBomb(up) === true) {
+    if (this.checked.indexOf(up.toString()) === -1) {
+      this.adjacentBlanks.push(up);
     }
   }
-  if (isInBounds(down) === true && isNotMine(down) === true) {
-    if (checked.indexOf("" + down) === -1) {
-      adjacentBlanks.push(down);
+  if (this.isInBounds(down) === true && this.isNotBomb(down) === true) {
+    if (this.checked.indexOf(down.toString()) === -1) {
+      this.adjacentBlanks.push(down);
     }
   }
-  if (isInBounds(left) === true && isNotMine(left) === true) {
-    if (checked.indexOf("" + left) === -1) {
-      adjacentBlanks.push(left);
+  if (this.isInBounds(left) === true && this.isNotBomb(left) === true) {
+    if (this.checked.indexOf(left.toString()) === -1) {
+      this.adjacentBlanks.push(left);
     }
   }
-  if (isInBounds(right) === true && isNotMine(right) === true) {
-    if (checked.indexOf("" + right) === -1) {
-      adjacentBlanks.push(right);
+  if (this.isInBounds(right) === true && this.isNotBomb(right) === true) {
+    if (this.checked.indexOf(right.toString()) === -1) {
+      this.adjacentBlanks.push(right);
     }
   }
 }
 
-function loopThroughBoard() {
-  for (var i = 0; i < adjacentBlanks.length; i++) {
-    if (checked.indexOf("" + adjacentBlanks[i]) === -1) {
-      checked.push("" + adjacentBlanks[i]);
-      var coordinates = "" + adjacentBlanks[i][0] + adjacentBlanks[i][1];
+Board.prototype.loopThroughBoard = function() {
+  for (var i = 0; i < this.adjacentBlanks.length; i++) {
+    if (this.checked.indexOf(this.adjacentBlanks[i].toString()) === -1) {
+      this.checked.push(this.adjacentBlanks[i].toString());
+      var coordinates = this.adjacentBlanks[i][0].toString() + "-" + this.adjacentBlanks[i][1].toString();
       $("#" + coordinates).toggleClass("coordinateHighlight");
-      pushAdjacents(coordinates);
+      this.pushAdjacents(coordinates.split("-"));
     }
   }
 }
 
-function getX(id) {
-  var x = parseInt(id.charAt(0));
-  return x;
-}
-
-function getY(id) {
-  var y = parseInt(id.charAt(1));
-  return y;
-}
-
-function isNotMine(coordinates) {
-  if (gameBoard.grid[coordinates[0]][coordinates[1]] != "X") {
+Board.prototype.isNotBomb = function(coordinates) {
+  if (this.grid[coordinates[0]][coordinates[1]] != "X") {
     return true;
   } else {
     return false;
   }
 }
 
-function isNotNumber(coordinates) {
-  if (gameBoard.grid[coordinates[0]][coordinates[1]] < 1) {
+Board.prototype.isInBounds = function(coordinates) {
+  if (coordinates[0] >= 0 && coordinates[0] < this.gridWidth &&
+      coordinates[1] >= 0 && coordinates[1] < this.gridWidth) {
     return true;
   } else {
     return false;
   }
 }
 
-function isInBounds(coordinates) {
-  if (coordinates[0] >= 0 && coordinates[0] < gameBoard.width &&
-      coordinates[1] >= 0 && coordinates[1] < gameBoard.width) {
-    return true;
-  } else {
-    return false;
-  }
+Board.prototype.clearArray = function() {
+  this.grid.length = 0;
 }
 
-function clearArray(array) {
-  array.length = 0;
-}
 
 var gameBoard = new Board();
-
 $(function() {
 
   $("form").submit(function(event) {
     event.preventDefault();
-    gridWidth = parseInt($("#gridDimension").val());
+    var gridWidth = parseInt($("#gridDimension").val());
 
-    gameBoard.width = gridWidth;
+    gameBoard.gridWidth = gridWidth;
     gameBoard.minesRemaining = gridWidth;
     gameBoard.resetGrid();
     gameBoard.placeMines();
