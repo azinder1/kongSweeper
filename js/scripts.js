dc92ff3bcd77c04929084200609db6b62161c65f
Levi Bibo <levibibo@gmail.com>
2016-11-06 16:22:07 -0800

    add method to set mine warning markers on grid.

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,34 @@ Board.prototype.placeMines = function() {
   for (mine = 0; mine < this.minesRemaining; mine++) {
     var locationIndex = Math.floor(Math.random() * potentialLocations.length);
     var randomMine = potentialLocations[locationIndex];
-    this.grid[randomMine[0]][randomMine[1]] = 1;
+    this.grid[randomMine[0]][randomMine[1]] = "X";
   }
-  console.log("grid: ", this.grid);
+}
+
+//add mine warning numbers to grid
+Board.prototype.setMineWarnings = function() {
+  //loop through grid
+  for (row = 0; row < this.width; row++) {
+    for (column = 0; column < this.width; column++) {
+      //check for mine at that location
+      if (this.grid[row][column] === "X") {
+        //loop through adjacent squares
+        for (rowAdjust = -1; rowAdjust < 2; rowAdjust++) {
+          for (columnAdjust = -1; columnAdjust < 2; columnAdjust++) {
+            //test to make sure the selected square is within the grid
+            if ((row+rowAdjust >= 0) && (row+rowAdjust <= this.width-1) && (column+columnAdjust >= 0) && (column+columnAdjust <= this.width-1)) {
+              console.log("count one");
+              //test to make sure the selected square doesn't also contain a mine
+              if (this.grid[row+rowAdjust][column+columnAdjust] != "X") {
+                //increment selected square by one
+                this.grid[row+rowAdjust][column+columnAdjust] += 1;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 Board.prototype.resetGrid = function() {
@@ -37,7 +62,7 @@ Board.prototype.updateUI = function() {
   for (row = 0; row < this.width; row++) {
     $(".grid").append('<div class="row gridRow" id="row' + row.toString() + '"></div>');
     for (column = 0; column < this.width; column++) {
-      $("#row" + row).append('<div class="gridColumn" id="' + row.toString()  + column.toString() + '">[]</div>');
+      $("#row" + row).append('<div class="gridColumn" id="' + row.toString()  + column.toString() + '">'+this.grid[row][column]+'</div>');
         if (this.grid[row][column]) {
           $("#"+row.toString()+column.toString()).toggleClass("hasMine");
         }
@@ -60,6 +85,7 @@ $(function() {
     gameBoard.minesRemaining = gridWidth;
     gameBoard.resetGrid();
     gameBoard.placeMines();
+    gameBoard.setMineWarnings();
     gameBoard.updateUI();
 
   });
